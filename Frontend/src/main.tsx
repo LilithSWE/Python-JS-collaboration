970c6a68ac56305bf2092eaf1e22f6531872b1c4
RalfiSlask <nilssonnarki@gmail.com>
2024-06-03 12:11:22 +0200

    🔧 improve right styles file

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 import { LoginContextProvider } from './context/LoginContext.tsx';
 import { ExcersisesContextProvider } from './context/ExcersisesContext.tsx';
+import './styles/style.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ExcersisesContextProvider>
