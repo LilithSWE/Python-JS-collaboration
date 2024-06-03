9d38c4bbeb86e0d9b4b13485a24dea889829d0aa
RalfiSlask <nilssonnarki@gmail.com>
2024-06-03 12:02:56 +0200

    ✨ use login and excersises providers to spread states in application

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { LoginContextProvider } from './context/LoginContext.tsx';
+import { ExcersisesContextProvider } from './context/ExcersisesContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <ExcersisesContextProvider>
+    <LoginContextProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </LoginContextProvider>
+  </ExcersisesContextProvider>
+);
