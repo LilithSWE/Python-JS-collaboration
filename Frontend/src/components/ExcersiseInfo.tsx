1b2a5772f2466a4a7c96ffa9e1e86a8e0b49ab6c
RalfiSlask <nilssonnarki@gmail.com>
2024-06-04 08:19:19 +0200

    ✨ add excersise info component

---
+++ Frontend/src/components/ExcersiseInfo.tsx
@@ -0,0 +1,17 @@
+import { getTextWithFirstLetterUpperCase } from '../assets/utils/helperfunctions/helperfunctions';
+
+interface IExcersiseInfoProps {
+  category: string;
+  text: string;
+}
+
+const ExcersiseInfo: React.FC<IExcersiseInfoProps> = ({ category, text }) => {
+  return (
+    <div className="flex items-center justify-between">
+      <p className="font-bold">{getTextWithFirstLetterUpperCase(category)}</p>
+      <p>{getTextWithFirstLetterUpperCase(text)}</p>
+    </div>
+  );
+};
+
+export default ExcersiseInfo;
