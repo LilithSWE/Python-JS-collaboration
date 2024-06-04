3f40c78a8563de13a11d10ffae4ebd28126f89a7
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-06-04 11:44:44 +0200

    :sparkles: adds header component

---
+++ Frontend/src/components/landing-page/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <>  
+        <header className="w-screen h-52 bg-blue-200 flex justify-center items-center">
+            <h1 className="font-sans text-3xl">Excercises</h1>
+        </header>
+        
+    </>
+  )
+}
+
+export default Header
