0228faa89d7547bc7b4ea8fb63fb29d7895a6139
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-06-04 12:04:49 +0200

    :sparkles: styles header

--- Frontend/src/components/landing-page/Header.tsx
+++ Frontend/src/components/landing-page/Header.tsx
@@ -1,8 +1,9 @@
 const Header = () => {
   return (
     <>  
-        <header className="w-screen h-52 bg-blue-200 flex justify-center items-center">
-            <h1 className="font-sans text-3xl">Excercises</h1>
+        <header className="w-screen h-52 bg-blue-200 flex justify-center items-center flex-col gap-3">
+            <h1 className="font-sans text-3xl">Excercises Company XL</h1>
+            <h2 className="font-sans">We provide you with excercises</h2>
         </header>
         
     </>
