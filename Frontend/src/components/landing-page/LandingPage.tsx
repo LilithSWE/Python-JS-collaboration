95d9cd662b29b579d5ea8440b4111e35348c453a
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-06-04 12:05:01 +0200

    :sparkles: imports components

--- Frontend/src/components/landing-page/LandingPage.tsx
+++ Frontend/src/components/landing-page/LandingPage.tsx
@@ -1,10 +1,14 @@
+import Footer from "./Footer";
 import Header from "./Header";
+import Main from "./Main";
 
 
 const LandingPage = () => {
   return (
     <>
       <Header /> 
+      <Main /> 
+      <Footer /> 
     </>
 
   )
