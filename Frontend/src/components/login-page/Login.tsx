b05dc5b8d62fb798419e58167f2e3489987fc6f2
RalfiSlask <nilssonnarki@gmail.com>
2024-06-05 17:24:00 +0200

    ✨ add login with header and inputs

--- Frontend/src/components/login-page/Login.tsx
+++ Frontend/src/components/login-page/Login.tsx
@@ -1,8 +1,33 @@
+import useLogin from '../../assets/custom-hooks/useLogin';
+import LoginButton from './LoginButton';
+import TextInput from './TextInput';
 
 const Login = () => {
-  return (
-    <button>Log In</button>
-  )
-}
+  const { userInput, passwordInput, handleChangeOnPasswordInput, handleChangeOnUserInput } = useLogin();
 
-export default Login
+  const inputs = [
+    { id: 1, text: 'username', value: userInput, changeInput: handleChangeOnUserInput },
+    { id: 2, text: 'password', value: passwordInput, changeInput: handleChangeOnPasswordInput },
+  ];
+
+  return (
+    <>
+      <section className="flex flex-col items-center gap-10">
+        <h2 className="uppercase text-3xl w-[300px] text-center">
+          <span className="text-orange">Make </span>
+          <span>Your Body </span>
+          <span className="text-purple">Healthy & Fit</span>
+        </h2>
+        <form className="w-full max-w-[400px] flex flex-col gap-4 items-center">
+          {inputs.map(input => {
+            const { id, text, value, changeInput } = input;
+            return <TextInput key={id} text={text} value={value} changeInput={changeInput} />;
+          })}
+          <LoginButton />
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default Login;
