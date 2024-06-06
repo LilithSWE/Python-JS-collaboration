4cbf90a34257e09c0b9f0dca8d6af4c9ef9cefbc
RalfiSlask <nilssonnarki@gmail.com>
2024-06-06 08:44:45 +0200

    💄 replace with custom color and prettier formatting

--- Frontend/src/components/landing-page/Header.tsx
+++ Frontend/src/components/landing-page/Header.tsx
@@ -1,13 +1,12 @@
 const Header = () => {
   return (
-    <>  
-        <header className="w-screen h-52 bg-blue-200 flex justify-center items-center flex-col gap-3">
-            <h1 className="font-sans text-3xl">Excercises Company XL</h1>
-            <h2 className="font-sans">We provide you with excercises</h2>
-        </header>
-        
+    <>
+      <header className="w-screen h-52 bg-lightblueColor flex justify-center items-center flex-col gap-3">
+        <h1 className="font-sans text-3xl">Excercises Company XL</h1>
+        <h2 className="font-sans">We provide you with excercises</h2>
+      </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
