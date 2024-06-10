362bc67d25f8f413690a8600079cf6bdbc546f89
RalfiSlask <nilssonnarki@gmail.com>
2024-06-10 12:19:32 +0200

    🚚 ✨ move interfaces and arrays for muscles and excersise to their own file, add selectedType state

--- Frontend/src/context/ExcersisesContext.tsx
+++ Frontend/src/context/ExcersisesContext.tsx
@@ -1,29 +1,11 @@
 import { ChangeEvent, createContext, ReactNode, useEffect, useState } from 'react';
-import data from '../data/excersises.json';
-import { getTextWithFirstLetterUpperCase } from '../assets/utils/helperfunctions/helperfunctions';
+import { IMuscle, IExcersise } from '../utils/types/types';
+import { musclesData, excersisesData } from '../data/excersisesData';
 
-interface IMuscle {
-  id: number;
-  muscle: string;
-}
-
-const getBackListOfObjectsWithIdAndKeyFromData = () => {};
-
-const musclesData = data
-  .map((excersise, index) => {
-    return { id: index, muscle: getTextWithFirstLetterUpperCase(excersise.muscle.replace('_', '')) };
-  })
-  .filter((muscle, index, muscles) => {
-    return index === muscles.findIndex(m => m.muscle === muscle.muscle);
-  });
-
-interface IExcersisesProps {
-  id: number;
-  excersise: string;
-}
+// Should probably refactor this later as a helperfunction to avoid the repeat
 
 interface IExcersisesContextProps {
-  types: IExcersisesProps[];
+  types: IExcersise[];
   muscles: IMuscle[];
   pickMuscleFromDropDown: (e: ChangeEvent<HTMLSelectElement>) => void;
 }
@@ -34,19 +16,11 @@ interface IContextProps {
 
 const ExcersisesContext = createContext<IExcersisesContextProps | undefined>(undefined);
 
-const excersises = data
-  .map((excersise, index) => ({
-    id: index + 1,
-    excersise: getTextWithFirstLetterUpperCase(excersise.type.replace('_', ' ')),
-  }))
-  .filter((type, index, types) => {
-    return index === types.findIndex(t => t.excersise === type.excersise);
-  });
-
 export const ExcersisesContextProvider: React.FC<IContextProps> = ({ children }) => {
   const [muscles, setMuscles] = useState<IMuscle[]>(musclesData);
   const [selectedMuscle, setSelectedMuscle] = useState(musclesData[0].muscle);
-  const [types, setTypes] = useState<IExcersisesProps[]>(excersises);
+  const [types, setTypes] = useState<IExcersise[]>(excersisesData);
+  const [selectedType, setSelectedType] = useState(excersisesData[0].excersise);
 
   const pickMuscleFromDropDown = (e: ChangeEvent<HTMLSelectElement>) => {
     setSelectedMuscle(e.target.value);
