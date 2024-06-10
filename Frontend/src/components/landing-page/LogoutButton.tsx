40c1d31a99e5904bfb3556a6cfd26158f4682a0d
RalfiSlask <nilssonnarki@gmail.com>
2024-06-10 08:39:03 +0200

    💄 add primary button styling

--- Frontend/src/components/landing-page/LogoutButton.tsx
+++ Frontend/src/components/landing-page/LogoutButton.tsx
@@ -3,7 +3,11 @@ import useLogin from '../../assets/custom-hooks/useLogin';
 const LogoutButton = () => {
   const { changeActivePage } = useLogin();
 
-  return <button onClick={() => changeActivePage('login')}>Logout</button>;
+  return (
+    <button className="primary-button" onClick={() => changeActivePage('login')}>
+      Logout
+    </button>
+  );
 };
 
 export default LogoutButton;
