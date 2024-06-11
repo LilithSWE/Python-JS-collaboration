ac460cb21b65fb83e0c301d5db5c1edb71cd3077
RalfiSlask <nilssonnarki@gmail.com>
2024-06-11 15:12:52 +0200

    ✨ wrap the app with create account context provider

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { LoginContextProvider } from './context/LoginContext.tsx';
 import { ExcersisesContextProvider } from './context/ExcersisesContext.tsx';
+import { CreateAccountContextProvider } from './context/CreateAccountContext.tsx';
 import './styles/style.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ExcersisesContextProvider>
-    <LoginContextProvider>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </LoginContextProvider>
-  </ExcersisesContextProvider>
+  <CreateAccountContextProvider>
+    <ExcersisesContextProvider>
+      <LoginContextProvider>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </LoginContextProvider>
+    </ExcersisesContextProvider>
+  </CreateAccountContextProvider>
 );
