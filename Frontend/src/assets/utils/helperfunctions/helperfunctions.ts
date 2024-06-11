23d9e42136691c86dd604639020296c13c018002
RalfiSlask <nilssonnarki@gmail.com>
2024-06-11 15:10:43 +0200

    ✨ add function for getting back a separated text

--- Frontend/src/assets/utils/helperfunctions/helperfunctions.ts
+++ Frontend/src/assets/utils/helperfunctions/helperfunctions.ts
@@ -1,5 +1,15 @@
 export const getTextWithFirstLetterUpperCase = (text: string) => {
-  const uppercaseText =
-    text.substring(0, 1).toUpperCase() + text.substring(1, text.length);
+  const uppercaseText = text.substring(0, 1).toUpperCase() + text.substring(1, text.length);
   return uppercaseText;
+};
+
+export const getSeperatedText = (text: string, includedText: string) => {
+  let newText = '';
+
+  if (text.includes(includedText)) {
+    newText = text.replace(includedText, ` ${includedText}`);
+    return newText;
+  } else {
+    return text;
+  }
 };
