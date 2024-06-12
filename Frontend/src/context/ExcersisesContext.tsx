95190d5ef0665c4ffc302020dbbaae3a1f231c35
RalfiSlask <nilssonnarki@gmail.com>
2024-06-12 11:01:30 +0200

    ✨ add state for excercises and a filter function when selecting types and muscles

--- Frontend/src/context/ExcersisesContext.tsx
+++ Frontend/src/context/ExcersisesContext.tsx
@@ -1,6 +1,7 @@
-import { ChangeEvent, createContext, ReactNode, useState } from 'react';
-import { IMuscle, IExcersiseObject } from '../assets/utils/types/types';
+import { ChangeEvent, createContext, ReactNode, useEffect, useState } from 'react';
+import { IMuscle, IExcersiseObject, ITrainingData } from '../assets/utils/types/types';
 import { musclesData, excersisesData } from '../data/excersisesData';
+import data from '../data/excersises.json';
 
 // Should probably refactor this later as a helperfunction to avoid the repeat
 
@@ -8,6 +9,7 @@ interface IExcersisesContextProps {
   types: IExcersiseObject[];
   muscles: IMuscle[];
   selectedType: string;
+  excercises: ITrainingData[];
   pickMuscleFromDropDown: (e: ChangeEvent<HTMLSelectElement>) => void;
   handleClickOnType: (type: string) => void;
 }
@@ -20,22 +22,34 @@ const ExcersisesContext = createContext<IExcersisesContextProps | undefined>(und
 
 export const ExcersisesContextProvider: React.FC<IContextProps> = ({ children }) => {
   const [muscles, setMuscles] = useState<IMuscle[]>(musclesData);
-  const [selectedMuscle, setSelectedMuscle] = useState(musclesData[0].muscle);
+  const [selectedMuscle, setSelectedMuscle] = useState(musclesData[0].muscle.toLowerCase());
   const [types, setTypes] = useState<IExcersiseObject[]>(excersisesData);
-  const [selectedType, setSelectedType] = useState(excersisesData[0].excersise);
+  const [selectedType, setSelectedType] = useState(excersisesData[0].excercise.toLowerCase());
+  const [excercises, setExcercises] = useState<ITrainingData[]>(data);
 
   const pickMuscleFromDropDown = (e: ChangeEvent<HTMLSelectElement>) => {
-    setSelectedMuscle(e.target.value);
+    setSelectedMuscle(e.target.value.toLowerCase());
   };
 
+  useEffect(() => {
+    const filteredExcercises = data.filter(excercise => {
+      const bySelectedMuscle = selectedMuscle === 'all' || excercise.muscle.toLowerCase().includes(selectedMuscle);
+      const bySelectedType = selectedType === 'all' || excercise.type.toLowerCase().includes(selectedType);
+      return bySelectedMuscle && bySelectedType;
+    });
+
+    setExcercises(filteredExcercises);
+  }, [selectedType, selectedMuscle]);
+
   const handleClickOnType = (type: string) => {
-    setSelectedType(type);
+    setSelectedType(type.toLowerCase());
   };
 
   const excersisesValues = {
     muscles: muscles,
     types: types,
     selectedType: selectedType,
+    excercises: excercises,
     pickMuscleFromDropDown: pickMuscleFromDropDown,
     handleClickOnType: handleClickOnType,
   };
