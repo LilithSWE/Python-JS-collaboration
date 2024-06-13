5e8c3484e8b2bd1d857feb213fdac2b475de06ea
RalfiSlask <nilssonnarki@gmail.com>
2024-06-13 08:31:46 +0200

    🚚 🔧 change path to setupTests

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/test/setupTests.ts',
+    setupFiles: './src/tests/setupTests.ts',
     reporters: ['default', 'html'],
   },
 });
