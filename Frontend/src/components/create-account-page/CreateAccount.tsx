bbe6d53d9984bb0fe992fc587808700d67429a36
RalfiSlask <nilssonnarki@gmail.com>
2024-06-20 13:55:41 +0200

    ⚰️ remove unneccesary commented code, already implemented in context now

--- Frontend/src/components/create-account-page/CreateAccount.tsx
+++ Frontend/src/components/create-account-page/CreateAccount.tsx
@@ -2,17 +2,10 @@
 import { ICreateInputs } from '../../assets/utils/types/types';
 import CreateAccountInput from './CreateAccountInput';
 import GoBackButton from './GoBackButton';
-
+import useCreateAccount from '../../assets/custom-hooks/useCreateAccount';
 
 const CreateAccount = () => {
-/*   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
-  const [formData, setFormData] = useState<{ [key: string]: string }>({
-    firstname: '',
-    lastname: '',
-    email: '',
-    password: '',
-  });  */
+  const { handleSubmit, error, loading } = useCreateAccount();
 
   const inputs = [
     { id: 1, type: 'firstname', text: 'firstname', placeholder: 'first name' },
@@ -21,48 +14,22 @@ const CreateAccount = () => {
     { id: 4, type: 'password', text: 'password', placeholder: 'password' },
   ];
 
-  /* const handleSubmit = async () => { 
-    try {
-      const response = await axios.post('http://XXX/user/add', formData, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      if (response.status === 201) {
-        console.log('Account created!', response.data);
-        alert("You are now a member and ready to log in! Yay! :)");
-      } else if (response.status === 409) {
-        setError('User already exists with this email.');
-      } else {
-        throw new Error(`Server returned status: ${response.status}`);
-      }
-
-      } catch (error) {
-        console.error('Something went wrong:', error);
-        alert("Something went terribly wrong... :(");
-        setError('Failed to create account. Please try again later.');
-      } finally {
-        setLoading(false);
-      }
-    };
- */
   return (
     <section className="flex flex-col items-center gap-10 mt-60">
       <h2 className="uppercase text-3xl w-[300px] text-center">Create Account</h2>
       <div className="w-full max-w-[400px] flex flex-col gap-4 items-center">
-      <form className="w-full max-w-[400px] flex flex-col gap-4 items-center">
-        {inputs.map(input => {
-          const { text, type, placeholder } = input;
-          return (
-            <CreateAccountInput
-              key={input.id}
-              text={text}
-              type={type as keyof ICreateInputs}
-              placeholder={placeholder}
-            />
-          );
-        })}
+        <form className="w-full max-w-[400px] flex flex-col gap-4 items-center">
+          {inputs.map(input => {
+            const { text, type, placeholder } = input;
+            return (
+              <CreateAccountInput
+                key={input.id}
+                text={text}
+                type={type as keyof ICreateInputs}
+                placeholder={placeholder}
+              />
+            );
+          })}
           <button className="primary-button" onClick={handleSubmit} disabled={loading}>
             {loading ? 'Creating...' : 'Create'}
           </button>
