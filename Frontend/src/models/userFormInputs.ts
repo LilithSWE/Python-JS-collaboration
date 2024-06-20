e23c8532f8198d33e9aa13a6595fe3f322f7b2c3
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-06-20 14:31:02 +0200

    :truck: moves "inputs" to userFormInputs

---
+++ Frontend/src/models/userFormInputs.ts
@@ -0,0 +1,8 @@
+const userFormInputs = [
+  { id: 1, type: 'firstname', text: 'firstname', placeholder: 'first name' },
+  { id: 2, type: 'lastname', text: 'lastname', placeholder: 'last name' },
+  { id: 3, type: 'email', text: 'email', placeholder: 'john_doe@mail.com' },
+  { id: 4, type: 'password', text: 'password', placeholder: 'password' },
+];
+
+export default userFormInputs; 
